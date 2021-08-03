df5d2f86c037e304e622e0d9f61bb7ff3296c42f
Rebecca Carr <reca9238@colorado.edu>
2021-08-03 15:53:52 -0600

    added barrier line creator

---
+++ Assets/Scripts/AddBarrier.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBarrier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool addFirst;
+    bool addSecond;
+    public GameObject barrier;
+    public GameObject dotPrefab;
+
+    Vector3 pos1;
+    Vector3 pos2;
+
+    void Start()
+    {
+        addFirst = false;
+        addSecond = false;
+    }
+
+    void CreateBarrier()
+    {
+        // GameObject barr = Instantiate(barrier, pos1, Quaternion.identity, transform);
+        //
+        // RectTransform rectTransform = barr.GetComponent<RectTransform>();
+        // rectTransform.anchorMin = new Vector2(0, 0);
+        // rectTransform.anchorMax = new Vector2(0, 0);
+        // rectTransform.anchoredPosition = pos1;
+        Vector2 relPos = pos1 - pos2;
+        Vector2 mid = (pos1 + pos2) / 2;
+
+        float dist = Vector3.Distance(pos1, pos2);
+
+
+        GameObject barr = Instantiate(barrier, mid, Quaternion.identity);
+        // correct angle
+        Vector2 normalizedDiff = relPos.normalized;
+        float dir = Mathf.Acos(normalizedDiff.x) * Mathf.Sign(normalizedDiff.y);
+        barr.transform.localEulerAngles = new Vector3(0,0,dir*180/Mathf.PI);
+        // correct size
+        barr.transform.localScale = new Vector3(dist, 1, 1);
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(addFirst || addSecond)
+        {
+            if(Input.GetButtonDown("LeftClick"))
+            {
+                Vector3 mousePos =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                Instantiate(dotPrefab, mousePos, Quaternion.identity);
+                if(addSecond)
+                {
+                    addSecond = false;
+                    pos2 = mousePos;
+                    CreateBarrier();
+                }
+                else
+                {
+                    addFirst = false;
+                    addSecond = true;
+                    pos1 = mousePos;
+                }
+
+            }
+
+
+
+        }
+    }
+
+    public void Add()
+    {
+        addFirst = true;
+    }
+}
